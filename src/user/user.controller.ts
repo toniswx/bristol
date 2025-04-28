30bbede9a472af5e9ffb4f0a582f6c3f1b858204
antonio <toninhoport@gmail.com>
2025-04-28 16:32:24 -0300

    added a public user data end point

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -14,16 +14,22 @@ import { User } from '@prisma/client';
 import { UserService } from './user.service';
 import createUserDto from './dto/create-one.dto';
 import deleteUserDTO from './dto/delete-user.dto';
-import { AuthGuard } from 'src/auth/auth.guard';
+import { UserDataDTO } from './dto/get-one.dto';
+import { ApiParam } from '@nestjs/swagger';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @UseGuards(AuthGuard)
   @Get(':id')
+  @ApiParam({
+    name: 'UUID',
+    required: true,
+    description: ' user id in UUID format ',
+    schema: { oneOf: [{ type: 'string' }] },
+  })
   @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
-  async getOne(@Param() params: string): Promise<User> {
+  async getPublicUserData(@Param() params: string): Promise<UserDataDTO> {
     const id = params['id'] as string;
     return await this.userService.getUserData(id);
   }
