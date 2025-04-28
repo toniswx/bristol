7dccb57287155c2d77722c3351e2a7a5193f54f3
antonio <toninhoport@gmail.com>
2025-04-28 16:35:20 -0300

    removed auth to get user data and turn the end-point into a authless end-point

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -14,22 +14,16 @@ import { User } from '@prisma/client';
 import { UserService } from './user.service';
 import createUserDto from './dto/create-one.dto';
 import deleteUserDTO from './dto/delete-user.dto';
-import { UserDataDTO } from './dto/get-one.dto';
-import { ApiParam } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(AuthGuard)
   @Get(':id')
-  @ApiParam({
-    name: 'UUID',
-    required: true,
-    description: ' user id in UUID format ',
-    schema: { oneOf: [{ type: 'string' }] },
-  })
   @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
-  async getPublicUserData(@Param() params: string): Promise<UserDataDTO> {
+  async getOne(@Param() params: string): Promise<User> {
     const id = params['id'] as string;
     return await this.userService.getUserData(id);
   }
