a734f1c8acff613390bc0a0ec0e824dad3a2ce02
antonio <toninhoport@gmail.com>
2025-04-28 19:21:07 -0300

    added a cache to store user sessions

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,6 +1,7 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from 'jsonwebtoken';
+import { SessionService } from '../session/session.service';
 
 export interface CustomRequest {
   userId?: any; // or whatever type your userId is
@@ -11,7 +12,10 @@ export interface CustomRequest {
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly JwtService: JwtService) {}
+  constructor(
+    private readonly JwtService: JwtService,
+    private readonly SessionService: SessionService,
+  ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<CustomRequest>();
 
@@ -19,17 +23,21 @@ export class AuthGuard implements CanActivate {
 
     if (!sessionToken) return false;
     try {
-      const isValidJWT: JwtPayload = await this.JwtService.verifyAsync(
-        sessionToken,
-        {
+      const isValidJWT: JwtPayload =
+        await this.JwtService.verifyAsync<JwtPayload>(sessionToken, {
           ignoreExpiration: true,
-        },
+        });
+
+      if (!isValidJWT) return false;
+
+      const isValid = await this.SessionService.verifySessionTokenInCache(
+        isValidJWT.sub!,
       );
 
+      if (!isValid) return false;
       request.userId = isValidJWT.sub;
 
-      if (isValidJWT) return true;
-      return false;
+      return true;
     } catch (err) {
       console.error(err);
       return false;
